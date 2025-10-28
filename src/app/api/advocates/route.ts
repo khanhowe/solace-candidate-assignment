6d89c20926e460f333069276a5fd05cf7c87f423
alaska113 <alaska113@gmail.com>
2025-10-27 19:34:46 -0700

    fix: Add stronger typing to GET method

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,11 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
+import { Advocate } from "./types";
 
-export async function GET() {
+export async function GET(): Promise<Response> {
   // Uncomment this line to use a database
   // const data = await db.select().from(advocates);
 
-  const data = advocateData;
+  const data: Advocate[] = advocateData;
 
   return Response.json({ data });
 }
